ff5d55ac692ad96b35459db19174acd09998f248
Gustavo <gustavocars@gmail.com>
2022-09-19 17:40:50 -0300

    Subida de archivos.
    Subida de archivos inicial.

---
+++ CuadradoDeN.py
@@ -0,0 +1,6 @@
+n = int(input("Intorude un número entero: "))
+contador = 1
+
+while contador <= n:
+    print("El cuadrado de {} es {}" .format(contador, contador ** 2))
+    contador = contador + 1
